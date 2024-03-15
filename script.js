5429ab95f258ff58c0758cfc07211aa65b0cc6d2
DeepP99 <160652318+DeepP99@users.noreply.github.com>
2024-03-14 22:14:29 -0400

    added movement event listener

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.addEventListener('keydown', function(spacebarDown) {
+
     if (spacebarDown.code === 'Space') {
         jump();
     }
+
 });
+
+document.addEventListener('keydown', function(arrowDown) {
+    const samurai = document.querySelector('.samurai');
+
+    if (arrowDown.code === 'ArrowLeft') {
+        moveLeft();
+    } else if (arrowDown.code === 'ArrowRight') {
+        moveRight();
+    }
+});
+
+
 
 function jump() {
     const samurai = document.querySelector('.samurai');
@@ -16,6 +30,11 @@ function jump() {
 function moveRight() {
     const samurai = document.querySelector('.samurai');
     samurai.style.animation = 'moveRightAnimation 0.5s linear forwards';
+}
+
+function moveLeft() {
+    const samurai = document.querySelector('.samurai');
+    samurai.style.animation = 'moveLeftAnimation 0.5s linear forwards';
 }
 
 
